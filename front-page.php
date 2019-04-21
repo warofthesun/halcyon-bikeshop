4ba099b7fd2300660a4812c0a41ec9f90ddfb005
Daniel Brown <fireflip@gmail.com>
2019-04-21 13:43:27 -0500

    style homepage blocks

--- front-page.php
+++ front-page.php
@@ -7,10 +7,6 @@
 
 						<main id="main" class="col-xs-12" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
 
-							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-
-							<article id="post-<?php the_ID(); ?>" <?php post_class( 'single-post' ); ?> role="article">
-								<div class="hero--image"><?php the_post_thumbnail('gallery-image'); ?></div>
 								<?php
 									// args
 									$args = array(
@@ -29,11 +25,17 @@
 
 									?>
 									<?php if( $the_query->have_posts() ): ?>
-										<ul>
+										<ul class="row block-navigation block-navigation__blocks">
 										<?php while( $the_query->have_posts() ) : $the_query->the_post(); ?>
-											<li>
+
+											<li class="col-xs-5 col-sm-4 block">
 												<a href="<?php the_permalink(); ?>">
-													<?php the_title(); ?>
+												<?php the_post_thumbnail('halcyon-front-page'); ?>
+												<div class="overlay">
+													<div>
+															<?php the_title(); ?>
+													</div>
+												</div>
 												</a>
 											</li>
 										<?php endwhile; ?>
@@ -41,21 +43,6 @@
 									<?php endif; ?>
 
 									<?php wp_reset_query();	 // Restore global post data stomped by the_post(). ?>
-								<header class="article-header">
-
-									<h1 class="h2 entry-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
-
-								</header>
-
-								<section class="entry-content">
-
-									<?php the_content(); ?>
-								</section>
-
-							</article>
-
-							<?php endwhile; endif; ?>
-
 
 						</main>
 
