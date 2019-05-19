801ec62be3d5aab3b34ce84f7538940fb5a12d01
Daniel Brown <fireflip@gmail.com>
2019-05-18 19:32:59 -0500

    Use acf image links on home page

--- front-page.php
+++ front-page.php
@@ -7,47 +7,31 @@
 
 						<main id="main" class="col-xs-12" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
 
-								<?php
-									// args
-									$args = array(
-										'numberposts'	=> -1,
-										'post_type'		=> array (
-											'page',
-											'post',
-											'image_links'
-										),
-										'meta_key'		=> 'include_on_front',
-										'meta_value'	=> true
-									);
+							<?php
+								// check if the repeater field has rows of data
+								if( have_rows('image_links', 'option') ): ?>
+									<ul class="row block-navigation block-navigation__blocks">
 
+									<?php while ( have_rows('image_links', 'option') ) : the_row(); ?>
+										<li class="col-xs-5 col-sm-4 block">
 
-									// query
-									$the_query = new WP_Query( $args );
+											<a href="<?php the_sub_field('link'); ?>" target="_blank">
+											<?php
+											$image = get_sub_field('image');
+											$size = 'halcyon-front-page'; ?>
 
-									?>
-									<?php if( $the_query->have_posts() ): ?>
-										<ul class="row block-navigation block-navigation__blocks">
-										<?php while( $the_query->have_posts() ) : $the_query->the_post(); ?>
-
-											<li class="col-xs-5 col-sm-4 block">
-												<?php if (get_field('external_link') ) : ?>
-													<a href="<?php the_field('external_link'); ?>" <?php if (get_field('open_in_new_tab') ) : ?>target="_blank"<?php endif;?>>
-													<?php else : ?>
-												<a href="<?php the_permalink(); ?>">
-												<?php endif; ?>
-												<?php the_post_thumbnail('halcyon-front-page'); ?>
-												<div class="overlay">
-													<div>
-															<?php the_title(); ?>
-													</div>
+											<?php echo wp_get_attachment_image( $image, $size ); ?>
+											<div class="overlay">
+												<div>
+														<?php the_sub_field('link_title'); ?>
 												</div>
-												</a>
-											</li>
-										<?php endwhile; ?>
-										</ul>
-									<?php endif; ?>
+											</div>
+											</a>
 
-									<?php wp_reset_query();	 // Restore global post data stomped by the_post(). ?>
+									 <?php endwhile; ?>
+										</li>
+									</ul>
+							<?php	else : endif; ?>
 
 						</main>
 
--- inc/acf/options.php
+++ inc/acf/options.php
@@ -21,10 +21,4 @@ if( function_exists('acf_add_options_page') ) {
 		'parent_slug'	=> 'theme-general-settings',
 	));
 
-	acf_add_options_sub_page(array(
-		'page_title' 	=> 'Image Based Links',
-		'menu_title'	=> 'Image Based Links',
-		'parent_slug'	=> 'theme-general-settings',
-	));
-
 }
